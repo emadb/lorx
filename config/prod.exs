241f2a8ac99828a3bff2e6cde6bd1489449b2b3d
Emanuele DelBono <emanuele@codiceplastico.com>
2025-01-16 18:23:00 +0100

    no check origin

--- config/prod.exs
+++ config/prod.exs
@@ -7,7 +7,8 @@ import Config
 # before starting your production server.
 config :lorx, LorxWeb.Endpoint,
   cache_static_manifest: "priv/static/cache_manifest.json",
-  server: true
+  server: true,
+  check_origin: false
 
 # Configures Swoosh API Client
 config :swoosh, api_client: Swoosh.ApiClient.Finch, finch_name: Lorx.Finch
