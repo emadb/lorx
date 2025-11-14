59e198b5f8b5a8b080c9818e684e5157d0c972ce
Emanuele DelBono <emanuele@codiceplastico.com>
2025-11-14 09:57:28 +0100

    improved log

--- lib/lorx/shelly/device_client.ex
+++ lib/lorx/shelly/device_client.ex
@@ -1,25 +1,36 @@
 defmodule DeviceClient do
+  require Logger
+
   def get_temp(ip) do
     case Tesla.get("http://#{ip}/rpc/Temperature.GetStatus?id=100") do
       {:ok, resp} ->
         Jason.decode!(resp.body) |> then(fn %{"id" => 100, "tC" => t} -> {:ok, t} end)
 
-      {:error, _} ->
-        {:error, :error}
+      {:error, e} ->
+        Logger.error("DeviceClient.get_temp(#{ip})", %{error: e})
+        {:ok, :error}
     end
   end
 
   def switch_on(ip) do
     case Tesla.get("http://#{ip}/rpc/Switch.Set?id=0&on=true") do
-      {:ok, _} -> {:ok, :heating}
-      {:error, _} -> {:ok, :error}
+      {:ok, _} ->
+        {:ok, :heating}
+
+      {:error, e} ->
+        Logger.error("DeviceClient.switch_on(#{ip})", %{error: e})
+        {:ok, :error}
     end
   end
 
   def switch_off(ip) do
     case Tesla.get("http://#{ip}/rpc/Switch.Set?id=0&on=false") do
-      {:ok, _} -> {:ok, :idle}
-      {:error, _} -> {:ok, :error}
+      {:ok, _} ->
+        {:ok, :idle}
+
+      {:error, e} ->
+        Logger.error("DeviceClient.switch_off(#{ip})", %{error: e})
+        {:ok, :error}
     end
   end
 
@@ -31,7 +42,8 @@ defmodule DeviceClient do
           true -> {:ok, :heating}
         end
 
-      {:error, _} ->
+      {:error, e} ->
+        Logger.error("DeviceClient.get_status(#{ip})", %{error: e})
         {:ok, :error}
     end
   end
