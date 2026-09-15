c87334ebfd51c761d23d255670cfd92212b783a4
Emanuele DelBono <emanuele@codiceplastico.com>
2026-09-15 10:57:07 +0200

    update version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Lorx.MixProject do
   def project do
     [
       app: :lorx,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.19.0",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
